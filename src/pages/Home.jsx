8beb4537e2097c45955a4f8e54c2a7a4f193c16b
Lauro-Santos <lauros034@gmail.com>
2023-07-29 00:20:01 -0300

    dinamização de criação de componentes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,43 @@
 import Promocoes from "components/Promocoes";
 import SecaoDestaque from "components/SecaoDestaque";
 import SecaoPrincipal from "components/SecaoPrincipal";
-
-import banner1 from "assets/images/bg-banner_modastreet.jpg";
-import banner2 from "assets/images/bg-banner_destaque_mes.jpg";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [produtos, setProdutos] = useState([]);
+  const [banners, setBanners] = useState([]);
+  const urlDB =
+    "https://raw.githubusercontent.com/Lauro-Santos/espiral-data/main/db.json";
+  console.log(banners);
+
+  async function fetchData() {
+    try {
+      const { data } = await axios.get(urlDB);
+      setProdutos(data.produtos);
+      setBanners(data.banners);
+    } catch (error) {
+      console.error("Erro ao carregar os dados:", error);
+    }
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <main>
       <SecaoPrincipal />
       <Promocoes />
-      <SecaoDestaque urlImage={banner1} text={"Moda Street"} />
-      <SecaoDestaque urlImage={banner2} text={"Destaque "} span={"do mês"} />
+      {banners.map((banner) => {
+        return (
+          <SecaoDestaque
+            key={banner.id}
+            urlBanner={banner.imagem}
+            text={banner.texto}
+            span={banner.span}
+            data={produtos}
+          />
+        );
+      })}
     </main>
   );
 };
