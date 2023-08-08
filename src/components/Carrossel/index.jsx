47fccedc7a37e8d3fd14b740f03421dc1b5a4dc6
Lauro-Santos <lauros034@gmail.com>
2023-08-08 02:14:02 -0300

    mudança para mobile first

--- src/components/Carrossel/index.jsx
+++ src/components/Carrossel/index.jsx
@@ -9,36 +9,38 @@ const Carrossel = ({ data }) => {
 
   const handleLeftClick = (e) => {
     e.preventDefault();
-    carrosel.current.scrollLeft -= (carrosel.current.offsetWidth - 70);
+    carrosel.current.scrollLeft -= carrosel.current.offsetWidth - 60;
   };
   const handleRigthClick = (e) => {
     e.preventDefault();
-    carrosel.current.scrollLeft += (carrosel.current.offsetWidth - 70);
+    carrosel.current.scrollLeft += carrosel.current.offsetWidth - 60;
   };
   return (
-    <>
+    <div className={style.carrosel__conteiner}>
+      <button className={style.btn__left} onClick={handleLeftClick}>
+        <img src={seta} alt="Seta para a esquerda" />
+      </button>
       <ul className={style.carrossel} ref={carrosel}>
         {data.map((produto) => {
           return (
-            <CardCarrossel key={produto.id} preco={produto.preco} image={produto.imagem}>
+            <CardCarrossel
+              key={produto.id}
+              preco={produto.preco}
+              image={produto.imagem}
+            >
               {produto.nome}
             </CardCarrossel>
           );
         })}
       </ul>
-      <div className={style.botoes}>
-        <button onClick={handleLeftClick}>
-          <img src={seta} alt="Seta para a esquerda" />
-        </button>
-        <button onClick={handleRigthClick}>
-          <img
-            src={seta}
-            alt="Seta para a esquerda"
-            style={{ transform: "rotate(180deg)" }}
-          />
-        </button>
-      </div>
-    </>
+      <button className={style.btn__rigth} onClick={handleRigthClick}>
+        <img
+          src={seta}
+          alt="Seta para a esquerda"
+          style={{ transform: "rotate(180deg)" }}
+        />
+      </button>
+    </div>
   );
 };
 export default Carrossel;
